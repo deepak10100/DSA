bbac154cdb7aaf53d3eb873a2f712fe08ce1e094
deepak10100 <deepakmalakar98185@gmail.com>
2025-07-27 12:22:00 +0530

    update

--- 04.cpp
+++ 04.cpp
@@ -3,6 +3,22 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout<<"Eddpak";
+    int n;
+    cout<<"Enter the number ";
+    cin>>n;
+    int i =1;
+    while (i<=n)
+    {
+        int j =1;
+        while (j<=n)
+        {
+            cout<<'*';
+            j = j+1;
+        }
+        cout<<endl;
+        
+        i = i+1;
+    }
+    
     return 0;
 }
