844fd58f3b3898a2071231aec86097307233146d
deepak10100 <deepakmalakar98185@gmail.com>
2025-09-12 23:23:31 +0530

    update

--- 22.cpp
+++ 22.cpp
@@ -1,8 +1,143 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main(int argc, char const *argv[])
 {
-    int age;
+    // int age;
+    // cin>>age;
+    // if (age>18)
+    // {
+    //     cout<<"you are adult";
+    // }else if(age<10){
+    //     cout<<"you are not adult";
+    // }
+
+    // int mark;
+    // cin >> mark;
+    // if (mark >= 80 && mark <= 100)
+    // {
+    //     cout << "A";
+    // }
+    // else if (mark >= 60 && mark <= 79)
+    // {
+    //     cout << "B";
+    // }
+    // else if (mark >= 50 && mark <= 59)
+    // {
+    //     cout << "C";
+    // }
+    // else if (mark >= 45 && mark <= 49)
+    // {
+    //     cout << "D";
+    // }
+    // else if (mark >= 25 && mark <= 44)
+    // {
+    //     cout << "E";
+    // }
+    // else 
+    // {
+    //     cout << "F";
+    // }
+
+    // int age;
+    // cin>>age;
+    // if (age<18)
+    // {
+    //     cout<<"not eligable for job";
+    // }
+    // else if (age>=18 && age<=54)
+    // {
+    //     cout<<"eligable for job";
+    // }
+    // else if (age>=55 && age<=57)
+    // {
+    //     cout<<"eligable for job but retirement soon";
+    // }
+    // else if (age>57)
+    // {
+    //     cout<<"retirement time";
+    // }
     
+    // int num;
+    // cin>>num;
+    // if (num>=0)
+    // {
+    //    if (num==0)
+    //    {
+    //     cout<<"zero";
+    //    }else{
+    //     cout<<"positive";
+    //    }
+       
+    // }else{
+    //     cout<<"negative";
+    // }
+    
+    // char op;
+    // int num1;
+    // int num2;
+    // cout<<"Enter the number 1 ";
+    // cin>>num1;
+    // cout<<"Enter the number 2 ";
+    // cin>>num2;
+    // cout<<"Enter the operator ";
+    // cin>>op;
+    // if (op=='+')
+    // {
+    //     cout<<"the sum is "<<num1+num2;
+    // }else if (op=='-')
+    // {
+    //     cout<<"the substract is "<<num1-num2;
+    // }else if (op =='*')
+    // {
+    //     cout<<"the mul is "<<num1*num2;
+    // }else{
+    //     cout<<"invalid input";
+    // }
+    
+    // int num1;
+    // int num2;
+    // int num3;
+
+    // cin>>num1;
+    // cin>>num2;
+    // cin>>num3;
+
+    // if (num1>num2)
+    // {
+    //     cout<<"num1 bada h num2 se "<<num1;
+    // }else if(num2>num3){
+    //     cout<<"num 2 se num 3 bada h " <<num2;
+    // }else{
+    //     cout<<"num 3 bada h " <<num3;
+    // }
+    
+    // int day;
+    // cout<<"Enter the day ";
+    // cin>>day;
+    // switch (day)
+    // {
+    // case 1:
+    //     cout<<"Sunday";
+    //     break;
+    // case 2:
+    //     cout<<"Monday";
+    //     break;
+    // case 3:
+    //     cout<<"Tuesday";
+    //     break;
+    
+    // default:
+    //     cout<<"invalid day";
+    //     break;
+    // }
+
+    // string number[3]={"deepak","deep","deepu"};
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     cout<<number[i]<<endl;
+    // }
+    
+    
+
     return 0;
 }
