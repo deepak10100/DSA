6291f2f037ecccb36f5401bd646236269601489d
deepak10100 <deepakmalakar98185@gmail.com>
2025-09-17 23:47:11 +0530

    learn

--- Pattern.cpp
+++ Pattern.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 using namespace std;
 void printStart(int n){
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-      for (int j = 0; j < i; j++)
+      for (int j = 1; j <= n-i+1; j++)
       {
         cout<<"*";
       }
@@ -11,6 +11,17 @@ void printStart(int n){
     }
     
 }
+// void printStart(int n){
+//     for (int i = 0; i < n; i++)
+//     {
+//       for (int j = 0; j < i; j++)
+//       {
+//         cout<<"*";
+//       }
+//         cout<<endl;
+//     }
+    
+// }
 // void printStart(int n){
 //     for (int i = 0; i < n; i++)
 //     {
