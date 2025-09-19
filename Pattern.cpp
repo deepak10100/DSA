ed9158f49fdca9542e3ea93c49f4c7b0497c9319
deepak10100 <deepakmalakar98185@gmail.com>
2025-09-19 23:36:54 +0530

    learn

--- Pattern.cpp
+++ Pattern.cpp
@@ -1,16 +1,56 @@
 #include<iostream>
 using namespace std;
 void printStart(int n){
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
-      for (int j = 1; j <= n-i+1; j++)
+      for (int j = 0; j < i; j++)
       {
-        cout<<j;
+        cout<<" ";
       }
-        cout<<endl;
+      for (int k = 0; k < 2*n -(2*i+1); k++)
+      {
+        cout<<"*";
+      }
+      for (int j = 0; j <i; j++)
+      {
+        cout<<" ";
+      }
+      cout<<endl;
+        
     }
     
 }
+void printStart1(int n){
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < n-i-1; j++)
+      {
+        cout<<" ";
+      }
+      for (int k = 0; k < 2*i+1; k++)
+      {
+        cout<<"*";
+      }
+      for (int j = 0; j < n-i-1; j++)
+      {
+        cout<<" ";
+      }
+      cout<<endl;
+        
+    }
+    
+}
+// void printStart(int n){
+//     for (int i = 1; i <= n; i++)
+//     {
+//       for (int j = 1; j <= n-i+1; j++)
+//       {
+//         cout<<j;
+//       }
+//         cout<<endl;
+//     }
+    
+// }
 // void printStart(int n){
 //     for (int i = 1; i <= n; i++)
 //     {
@@ -48,6 +88,7 @@ int main(int argc, char const *argv[])
 {
     int n;
     cin>>n;
+    printStart1(n);
     printStart(n);
     return 0;
 }
