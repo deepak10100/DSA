feca27ad153549e22bf2dcf269fd13f0b90c9f9f
deepak10100 <deepakmalakar98185@gmail.com>
2025-09-25 00:45:51 +0530

    learn

--- Pattern.cpp
+++ Pattern.cpp
@@ -20,27 +20,118 @@ using namespace std;
 //     }
     
 // }
+// void printStart1(int n){
+//   int count=1;
+//   for (int i = 1; i <= n; i++)
+//   {
+//     for (int j = 1; j <= i; j++)
+//     {
+//       cout<<count;
+//       count++;
+//     }
+//     cout<<endl;
+//   }
+  
+   
+// }
+// void printStart1(int n){
+//   char count='A';
+//   for (int i = 1; i <= n; i++)
+//   {
+//     for (int j = 1; j <= i; j++)
+//     {
+//       cout<<count;
+//       count++;
+//     }
+//     cout<<endl;
+//   }
+// }
 void printStart1(int n){
-   int start =1;
-   for (int i = 0; i < n; i++)
-   {
-    if (i%2 ==0)
+  for (int i = 0; i <= n; i++)
+  {
+    char count='A' +i;
+    for (int j = 0; j <= i; j++)
     {
-      start=1;
-    }else{
-      start=0;
-    }
-    for (int j = 0; j <=i; j++)
-    {
-      cout<<start;
-      start=1-start;
+      cout<<count;
+      // count++;
     }
     cout<<endl;
-    
-   }
-   
-    
+  }
+  
 }
+// void printStart1(int n){
+//   for (int i = 1; i <= n; i++)
+//   {
+//     char count='A';
+//     for (int j = 1; j <= n-i+1; j++)
+//     {
+//       cout<<count;
+//       count++;
+//     }
+//     cout<<endl;
+//   }
+  
+// }
+// void printStart1(int n){
+//   for (int i = 1; i <= n; i++)
+//   {
+//     char count='A';
+//     for (int j = 1; j <= i; j++)
+//     {
+//       cout<<count;
+//       count++;
+//     }
+//     cout<<endl;
+//   }
+  
+   
+// }
+// void printStart1(int n){
+//   int space = 2*(n-1);
+//   for (int i = 1; i <=n; i++)
+//   {
+//     for (int j = 1; j <=i; j++)
+//     {
+//       cout<<j;
+//     }
+//     for (int j = 1; j <= space; j++)
+//     {
+//       cout<<" ";
+//     }
+//     for (int j = i; j >=1; j--)
+//     {
+//       cout<<j;
+//     }
+//     cout<<endl;
+//     space -=2;
+    
+    
+    
+//   }
+  
+    
+   
+// }
+// void printStart1(int n){
+//    int sart =1;
+//    for (int i = 0; i < n; i++)
+//    {
+//     if (i%2 ==0)
+//     {
+//       sart =1;
+//     }
+//     else{
+//       sart=0;
+//     }
+//     for (int j = 0; j <=i; j++)
+//     {
+//       cout<<sart;
+//       sart=1-sart;
+//     }
+//     cout<<endl;
+    
+//    } 
+// }
 // void printStart(int n){
 //     for (int i = 1; i <= n; i++)
 //     {
