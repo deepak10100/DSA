7f78a0c70bcea8bf56770ada6cc9ab40f76c98b3
deepak10100 <deepakmalakar98185@gmail.com>
2025-10-07 23:25:52 +0530

    learning

--- stllibary.cpp
+++ stllibary.cpp
@@ -1,50 +1,123 @@
 #include<iostream>
 #include<vector>
-#include<list>
-#include<stack>
-#include<queue>
-#include<deque>
-#include<set>
-#include<map>
+#include<math.h>
 #include<algorithm>
+#include<map>
 using namespace std;
 int main(int argc, char const *argv[])
 {
-//     map<string,int>personal={{"Deepak",12},{"aman",15},{"deep",23}};
-//    for(auto str:personal){
-//     cout<<str.first <<" "<<str.second<<" ";
-
-//    }
-
-vector<int>cars={1,2,10,3,4,7,6};
-// sort(cars.begin(),cars.end());
-auto it = find(cars.begin(), cars.end(),3);
-
-if (it!=cars.end())
-{
-    cout<<"found";
-}else{
-    cout<<"not found";
-}
-
-   
-//    set<string> cars ={"Lambo","Bmw","RR","Porche"} ;
-// set<int>number={1,2,3,4,5,6,7};
-// number.clear();
-//    car.push_back("Deepak");
-//    car.push_back("Deepak");
-//    car.push_back("Deepak");
-//    car.push_back("Deepak");
-   
-//    car.back()="Deepak";
-//    car.push_back("Depl");
-//    car.push_front("De");  
-//    car.pop_back();
-//    cout<<car.size()<<endl;
-//    for(string str:car){
-//     cout<<str<<endl;
-//    }
+    // map<int,string>mp={{1,"Deepak"},{2,"deep"},{3,"dpk"}};
+    // cout<<mp[2];
+    // auto it = mp.find(6);
+    // if (it != mp.end())
+    // {
+    //     cout<<"found";
+    // }else{
+    //     cout<<"not found";
+    // }
     
-    
+    // mp.insert({4,"Deepk"});
+    // mp.erase(3);
+    // for(auto p:mp){
+    //     cout<<p.first<<"=>"<<p.second<<endl;
+    // }
+    // vector<int>v={1,2,3,4};
+    // v.erase(v.begin()+2);
+    // cout<<v.size();
+    // v.capacity();
+    // v.pop_back();
+    // vector<int>v(10,7);
+    // v.push_back(3);
+    // v.push_back(4);
+    // v.push_back(5);
+    // v.push_back(6);
+    // cout<<v.size()<<endl;
+    // cout<<v.clear();
+    // cout<<v.empty();
+    // v.insert(v.begin(),5);
+    // cout<<v.at(0);
+    // cout<<v.back();
+    // cout<<v.front();
+//    for (auto i = 0; i < v.size(); i++)
+//    {
+//     cout<<v[i]<<endl;
+//    }
+//    for(auto i:v){
+//     cout<<i<<endl;  
+//    }
+// for (auto i = v.begin(); i != v.end(); i++)
+// {
+//     cout<<*i;
+// }
+// vector<int>marks;
+// int n,mark;
+// cout<<"Enter the number " <<endl;
+// cin>>n;
+// for (int i = 1; i <=n; i++)
+// {
+//    cout<<"Enter the Marks: "<<endl;
+//    cin>>mark;
+//    marks.push_back(mark);
+// }
+
+// cout<<"Your Marks: "<<endl;
+// for(auto m:marks){
+//     cout<<m<<" ";
+// }
+
+// cout<<"Highest marks: "<<*max_element(marks.begin(),marks.end());
+
+// map<int,int>marks;
+// int n,mark,roll;
+// cout<<"Enter the number ";
+// cin>>n;
+// for (int i = 1; i <=n; i++)
+// {
+//     cout<<"Enter the Roll No and Marks ";
+//     cin>>mark>>roll;
+//     marks.insert({roll,mark});
+// }
+
+// for(auto it:marks){
+//     cout<<"Roll: "<<it.first<<"=>"<<it.second<<endl;
+// }
+// cout<<"Enter the roll Number ";
+// cin>>roll;
+// auto it = marks.find(roll);
+// if (it != marks.end())
+// {
+//     cout<<"Found "<<it->second;
+// }else{
+//     cout<<"Not found";
+// }
+
+// vector<int>v={1,4,3,2,5};
+// int key = 8;
+// auto it = find(v.begin(),v.end(),key);
+// int it =count(v.begin(),v.end(),2);
+// reverse(v.begin(),v.end());
+// for(auto it :v){
+//     cout<<it;
+// }
+// if (it != v.end())
+// {
+//     cout<<"found";
+// }else{
+//     cout<<"Not found";
+// }
+
+// sort(v.rbegin(),v.rend());
+// for(auto it:v){
+//     cout<<it;
+// }
+// for (auto it = v.rbegin(); it != v.rend(); it++)
+// {
+//     cout<<*it<<endl;
+// }
+
+// for(auto it:v){
+//     cout<<it;
+// }
+
     return 0;
 }
