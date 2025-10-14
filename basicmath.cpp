2f6496a660d72cc1e05f3fbee4ccce08fa6c7713
deepak10100 <deepakmalakar98185@gmail.com>
2025-10-15 01:24:49 +0530

    learn

--- basicmath.cpp
+++ basicmath.cpp
@@ -1,16 +1,36 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 int main(int argc, char const *argv[])
 {
     int n;
     cin>>n;
+    int og =n;
+    int rev=0;
     int count=0;
     while (n!=0)
     {
-        n = n/10;
+        int lg = n%10;
+        n=n/10;
+        rev = (rev*10)+lg;
         count++;
     }
-    cout<<count;
+    cout<<rev<<endl;
+    if (rev==og)
+    {
+        cout<<"pr";
+    }else{
+        cout<<"Not pr";
+    }
+    
+
+    // int n;
+    // cin>>n;
+    // int count=0;
+    // while (n!=0)
+    // {
+    //     n=n/10;
+    //     count++;
+    // }
+    // cout<<count;
     return 0;
 }
