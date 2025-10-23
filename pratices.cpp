26f6a8e9f267141fcb7c42918405df7c5cbce915
deepak10100 <deepakmalakar98185@gmail.com>
2025-10-23 22:27:10 +0530

    learn

--- pratices.cpp
+++ pratices.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 using namespace std;
+void f(int arr[], int l, int r){
+    if (l>=r)
+    {
+        return;
+    }
+    swap(arr[l],arr[r]);
+    f(arr, l+1, r-1);
+    
+}
 int main(int argc, char const *argv[])
 {
-    int n;
-    cin >> n;
-    for (int i = 1; i <= n; i++)
+    int n=5;
+    int arr[n]={1,2,3,4,5};
+
+    f(arr,0,n-1);
+    for (int i = 0; i < n; i++)
     {
-        char ch ='E'-i+1;
-        for (int j = 1; j <= i; j++)
-        {
-        cout<<ch;
-        ch++;
-       }
-       
-       
-        cout << endl;
+        cout<<arr[i];
     }
+    
 
     return 0;
 }
--- recursion.cpp
+++ recursion.cpp
@@ -54,16 +54,57 @@ int func5(int i){
     
     return i*func5(i-1);
 }
-
+void func6(int arr[],int l, int r){
+    if (l>=r)
+    {
+        return;
+    }
+    swap(arr[l],arr[r]);
+    func6(arr, l+1,r-1);
+}
+void func7(string n , int i){
+   if (i<0)
+   {
+    return;
+   }
+   cout<<n[i];
+   func7(n,i-1);
+}
+int func8(int n){
+    if (n==1)
+    {
+        return 1;
+    }
+    if (n==0)
+    {
+        return 0;
+    }
+    return (func8(n-1)+ func8(n-2));
+    
+    
+}
 int main(int argc, char const *argv[])
 {
-    int n;
-    cin>>n;
+    int n=5;
+    for (int i = 0; i < n; i++)
+    {
+        cout<<func8(i);
+    }
+    
+//    string n="Deepak";
+//    func7(n, n.size()-1);
+    // int arr[]={1,2,3,4,5};
+    // int n=5;
+    // func6(arr, 0,n-1);
    
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout<<arr[i]<<" ";
+    // }
     
     // func1(1,n);
     // int d = ;
-    cout<<func5(n);
+    // cout<<func5(n);
     // func4(n,0);
     // func3(n,n);
     // func2(1,n);
