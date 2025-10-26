b129b3d04273914b586a059b666eb1261dc21427
deepak10100 <deepakmalakar98185@gmail.com>
2025-10-26 23:50:36 +0530

    learning

--- pratices.cpp
+++ pratices.cpp
@@ -1,55 +1,93 @@
 #include<iostream>
 using namespace std;
 #include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<map>
+
 int main(int argc, char const *argv[])
 {
-    vector<int>v(5,7);
-    v.resize(8);
-    // v.assign(7,7);
-    // v.insert(v.begin(),9);
+    map<string,int>p={{"Deep",1},{"dee",2}};
+    p.insert({"ddd",3});
+    p.emplace("Dd",33);
+    // p.erase("Deep");
+    if (p.find("Deep")!=p.end())
+    {
+        cout<<"found";
+    }
+    
+    // cout<<p.count("dee")<<endl;
+    for(auto it:p){
+        cout<<it.first<<" "<<it.second<<endl;
+    }
 
-for (int i = 0; i < v.size(); i++)
-{
-    cout<<v[i]<<" ";
-}
-
-    // int n;
-    // int sum =0;
-    // for (int i = 0; i < 5; i++)
-    // {
-    //     cin>>n;
-    // v.push_back(n);
-    //     sum = sum+n;
-    // }
-    // cout<<sum;
     
-    // for (int i = 0; i < v.size(); i++)
+    // priority_queue<int,vector<int>, greater<int>>p;
+//     priority_queue<int>p;
+    // p.push(1);
+    // p.push(2);
+    // p.push(3);
+    // p.push(4);
+    // cout<<p.top();
+//     p.pop();
+// cout<<p.size();
+    // stack<int>s;
+    // s.push(1);
+    // s.push(2);
+    // s.push(3);
+    // s.push(4);
+    // s.push(5);
+    
+    // cout<<s.top();
+    // vector<pair<int,int>>p={{1,2},{3,40},{4,50}};
+    // p.emplace_back(7,7);
+    // p.push_back({4,5});
+    // for(auto it:p){
+    //     cout<<it.first<<" "<<it.second<<endl;
+    // }
+    // cout<<p.second.second;
+    // pair<int,int>p={1,2};
+    // pair<string,int>p={"Deep",2};
+    // cout<<p.second;
+    // cout<<p.first;
+    // list<int>l;
+    // l.push_back(1);
+    // l.push_front(7);
+    // l.push_front(2);
+    // l.push_front(8);
+    // l.push_back(6);
+    // l.pop_back();
+    // l.pop_front();
+    // for(int lec:l){
+    //     cout<<lec<<" ";
+    // }
+    // vector<int>vec(7,7);
+    // vector<int>vec={1,2,3,4,5,6,7};
+    // vec.insert(vec.begin()+2,100);
+    // vec.erase(vec.begin(),vec.begin()+3);
+    // cout<<vec.at(2);
+    // cout<<vec[2];
+    // vec.push_back(1);
+    // vec.push_back(2);
+    // vec.push_back(3);
+    // vec.push_back(4);
+    // vec.push_back(5);
+    // vec.push_back(6);
+    // vec.push_back(7);
+    // vec.pop_back();
+    // cout<<vec.front();
+    // cout<<vec.empty();
+    // cout<<vec.capacity();
+    // cout<<vec.size();
+    // cout<<vec.back();
+    // for(int v:vec){
+    //     cout<<v<<endl;
+    // }
+    // for (auto i = vec.begin(); i != vec.end(); i++)
     // {
-    //     cout<<v[i]<<" ";
+    //     cout<<*(i)<<" ";
     // }
     
-    // for (auto i = v.begin(); i != v.end(); i++)
-    // {
-    //     cout<<*i<<" ";
-    // }
-    
-    // vector<int>v={1,2,3,4,5};
-    // v.push_back(7);
-    // v.pop_back();
-    // v.clear();
-    // cout<<v.clear<<endl;
-    // cout<<v[2]<<endl;
-    // cout<<v.at(2)<<endl;
-    // cout<<v.back()<<endl;
-    // cout<<v.front()<<endl;
-    // cout<<v.size()<<endl;
-    // v.erase(v.begin()+3);
-    // v.assign(5,7);
-    // cout<<v.max_size();
-    // cout<<*v.data()<<endl;
-    // v.insert(v.begin()+3,99);
-    // for(int n:v){
-    //     cout<<n<<" ";
-    // }
     return 0;
 }
