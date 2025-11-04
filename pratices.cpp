66bfedad98597e1355cd994f56b18dd2ae5a0a15
deepak10100 <deepakmalakar98185@gmail.com>
2025-11-04 23:11:12 +0530

    learning

--- pratices.cpp
+++ pratices.cpp
@@ -1,30 +1,106 @@
 #include<iostream>
 using namespace std;
+void fun(int a){
+    a=a+7;
+}
+// void fun(int a){
+//     cout<<a+7;
+// }
 int main(int argc, char const *argv[])
 {
-    int mark;
-    cin>>mark;
-    if (mark>=25 && mark<=44)
-    {
-        cout<<"Grade E";
-    }else if (mark>=45 && mark<=49)
-    {
-        cout<<"Grade D";
-    }else if (mark>=50 && mark<=59)
-    {
-        cout<<"Grade C";
-    }
-    else if (mark>=60 && mark<=69)
-    {
-        cout<<"Grader B";
-    }else if (mark>=70 && mark<=100)
-    {
-        cout<<"Grade A";
-    }else{
-        cout<<"Fail";
-    }
+    int n=3;
+    fun(n);
+    cout<<n;
+    // int day=5;
+    // switch (day)
+    // {
+    // case 1:
+    //     cout<<"Monday";
+    //     break;
+    // case 2:
+    //     cout<<"tuesday";
+    //     break;
+    // case 3:
+    //     cout<<"wednesday";
+    //     break;
+    // case 4:
+    //     cout<<"thursday";
+    //     break;
+    // case 5:
+    //     cout<<"friday";
+    //     break;
+    // case 6:
+    //     cout<<"saturday";
+    //     break;
+    // case 7:
+    //     cout<<"sunday";
+    //     break;
     
+    // default:
+    //     cout<<"Invalid number";
+    //     break;
+    // }
+    // int arr[5]={1,2,3,4,5};
+    // // cout<<arr[3];
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout<<arr[i];
+    // }
+    // string s;
+    // cin>>s;
+    // // cout<<s.length();
+    // for (int i = 0; i < s.length(); i++)
+    // {
+    //     cout<<s[i]<<endl;
+    // }
     
+    // string s;
+    // cin>>s;
+    // string s1;
+    // cin>>s1;
+    // if (s==s1)
+    // {
+    //     cout<<"same: ";
+    // }else{
+    //     cout<<"Not same";
+    // }
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout<<"Deepak"<<endl;
+    // }
+    
+    // for (int i = 0; i < 7; i++)
+    // {
+    //     for (int j = 0; j < 7; j++)
+    //     {
+    //         cout<<"i: "<<i<<" j: "<<j<<endl;
+    //     }   
+    // }
+    // for (int i = 0; i < 7; i++)
+    // {
+    //     if (i%2==0)
+    //     {
+    //         cout<<"Even"<<endl;
+    //     }else{
+    //         cout<<"Odd"<<endl;
+    //     }
+        
+    // }
+
+    // int n=5;
+    // int fec =1;
+    // while (n>0)
+    // {
+    //     fec*=n;
+    //     n--;
+    // }
+    // cout<<"fac "<<fec;
+    // int i=1;
+    // while (i<=10)
+    // {
+    //     cout<<i;
+    //     i++;
+    // }
     
     
     
