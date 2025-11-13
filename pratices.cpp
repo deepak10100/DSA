2f89f2403e87a9269fbd6b56c489b0c10c5ea82a
deepak10100 <deepakmalakar98185@gmail.com>
2025-11-13 21:06:30 +0530

    learning

--- pratices.cpp
+++ pratices.cpp
@@ -1,31 +1,33 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-
-void selection_sort(int arr[], int n) {
-    for(int i = 0; i <= n-2; i++) {
-        int mini = i;
-        for(int j = i; j <= n-1; j++) {
-            if(arr[j] < arr[mini]) {
-                mini = j;
-            }
-        }
-        int temp = arr[mini];
-        arr[mini] = arr[i];
-        arr[i] = temp;
-    }
-}
-
-int main() {
+int main(int argc, char const *argv[])
+{
     int n;
-    cin >> n;
+    cin>>n;
     int arr[n];
-    for(int i = 0; i < n; i++)
-        cin >> arr[i];
-
-    selection_sort(arr, n);
-
-    for(int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
     }
+    for (int i = 0; i < n-1; i++)
+    {
+        int minindex=i;
+        for (int j = i+1; j < n; j++)
+        {
+           if (arr[j]<arr[minindex])
+           {
+            minindex=j;
+           }
+        }
+        swap(arr[minindex],arr[i]);
+    }
+    
+    //printArray
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+    
     return 0;
 }
