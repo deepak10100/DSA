072450006af77102bc62d297f1fdd97c661212f7
deepak10100 <deepakmalakar98185@gmail.com>
2025-11-25 22:32:18 +0530

    learning

--- pratices.cpp
+++ pratices.cpp
@@ -3,10 +3,22 @@ using namespace std;
 void print1(int n){
     for (int i = 1; i <=n; i++)
     {
-        for (int j = 1; j <=n-i+1; j++)
+        for (int j = 1; j <= i; j++)
         {
-            cout<<j;
+            cout<<"*";
         }
+        
+        
+       cout<<endl; 
+    } 
+    for (int i = n; i >=1; i--)
+    {
+        for (int j = i; j >= 1; j--)
+        {
+            cout<<"*";
+        }
+        
+        
        cout<<endl; 
     } 
 }
