66e9d5981ff645c1ba59581c75d55e532b7c2def
deepak10100 <deepakmalakar98185@gmail.com>
2025-12-02 03:10:41 +0530

    learning

--- pratices.cpp
+++ pratices.cpp
@@ -1,21 +1,35 @@
 #include<iostream>
-#include<vector>
+#include<map>
 using namespace std;
-int fun(int n){
-    if (n<=1)
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
     {
+        cin>>arr[i];
+    }
     
-        return n;
+    map<int,int>mpp;
+    for (int i = 0; i < n; i++)
+    {
+        mpp[arr[i]]+=1;
+    }
+    int q;
+    cin>>q;
+    while (q--)
+    {
+        int number;
+        cin>>number;
+        cout<<mpp[number];
     }
     
     
-    return fun(n-1)+fun(n-2);
-}
-int main(int argc, char const *argv[])
-{
-  int n;
-  cin>>n; 
-  int m =fun(n);
-  cout<<m;
+
+    
+    
+    
+    
     return 0;
 }
