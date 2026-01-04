c2702890dad5b8d647df1b70e5591232e3eb1620
deepak10100 <deepakmalakar98185@gmail.com>
2026-01-04 23:03:13 +0530

    learning

--- pratices.cpp
+++ pratices.cpp
@@ -1,32 +1,41 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int main(int argc, char const *argv[])
 {
-    int n;
-    cin>>n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin>>arr[i];
-    }
-    for (int j = 0; j < n; j++)
-    {
-        for (int k = 0; k < n-j; k++)
-        {
-           if (arr[k]>arr[k+1])
-           {
-            swap(arr[k],arr[k+1]);
-           }
-        }
-       
-        
-    }
-    for (int i = 0; i < n; i++)
-    {
-        cout<<arr[i]<<" ";
-    }
+  string food = "Pizza";
+  string *meat = &food;
+  *meat="Number";
+  // meat = "Burger";
+  cout<<*meat;
+  // cout<<meat;
+  // string letter[2][4]={
+  //   {"A","B","c","D"},
+  //   {"E","F","G","H"}
+  // };
+  // for (int i = 0; i < 2; i++)
+  // {
+  //   for (int j = 0; j < 4; j++)
+  //   {
+  //     cout<<letter[i][j]<<endl;
+  //   }
     
-    
-    
-    return 0;
+  // }
+  
+  // vector<string>st;
+  // st.push_back("Deep");
+  // st.push_back("Deep1");
+  // cout<<st.size();
+//  string cars[4]={"De","D","m","y"};
+//  for(auto in:st){
+//   cout<<in<<" ";
+//  }
+//  for (int i = 0; i < 4; i++)
+//  {
+//   cout<<cars[i]<<" ";
+//  }
+ 
+
+  
+  return 0;
 }
