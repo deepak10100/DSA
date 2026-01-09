066b4f5bb90a45ac19450c114d1d86754840a4a7
deepak10100 <deepakmalakar98185@gmail.com>
2026-01-10 00:02:22 +0530

    learning

--- pratices.cpp
+++ pratices.cpp
@@ -1,96 +1,25 @@
 #include<iostream>
-#include<vector>
 using namespace std;
-class Employee{
-private:
-  string name;
-  float salary;
-public:
-  Employee(string name, float salary){
-    this->name=name;
-    this->salary=salary;
-  }
-  string getName(){return name;}
-  float getSalary(){return salary;}
-
-  void setName(string name){this->name=name;}
-  void setSalary(float salary){this->salary=salary;}
-
-  void displayDetails(){
-    cout<<"Employee "<<name<<endl;
-    cout<<"Salary "<<salary<<endl;
-  }
-
-};
-// void hello(){
-//   cout<<"Deepak";
-// }
-// int square(int x){
-//   return x*x;
-// }
-// int add(int a, int b){
-//   return a+b;
-// }
 int main(int argc, char const *argv[])
 {
-  Employee emp("Geek",100.0f);
-  emp.displayDetails();
-//  int x =10;
-//  int &add=x;
-//  cout<<"the value : "<<&x<<endl;
-//  cout<<"add : "<<&add<<endl;
-//  cout<<"value: "<<add<<endl;
-
-  // int arr[4]={1,2,3,4};
-  // cout<<arr[3];
-  // for (int i = 0; i < 4; i++)
-  // {
-  //   cout<<arr[i]<<endl;
-  // }
+  int a=123;
+  int sum =0;
+  while (a>0)
+  {
+    sum = sum+a%10;
+    a=a/10;
+  }
+  cout<<sum;
   
-  // int result = add(1,2);
-// hello();
-// int result = square(5);
-// cout<<result;
-  // string food = "Pizza";
-  // string *meat = &food;
-  // *meat="Number";
-  // meat = "Burger";
-  // cout<<*meat;
-  // cout<<meat;
-  // string letter[2][4]={
-  //   {"A","B","c","D"},
-  //   {"E","F","G","H"}
-  // };
-  // for (int i = 0; i < 2; i++)
-  // {
-  //   for (int j = 0; j < 4; j++)
-  //   {
-  //     cout<<letter[i][j]<<endl;
-  //   }
-    
-  // }
-  
-  // vector<string>st;
-  // st.push_back("Deep");
-  // st.push_back("Deep1");
-  // cout<<st.size();
-//  string cars[4]={"De","D","m","y"};
-//  for(auto in:st){
-//   cout<<in<<" ";
-//  }
-//  for (int i = 0; i < 4; i++)
+//   int rev=0;
+// //  int cnt=0;
+//  while (a>0)
 //  {
-//   cout<<cars[i]<<" ";
+//   rev = rev*10+a%10;
+//   a=a/10;
+//   // cnt=cnt+1;
 //  }
- 
-// cout<<"Deepak";
-// int num=1;
-// int num2=23;
-// // cout<<"Enter the number ";
-// swap(num,num2);
-// cout<<"Enter number is num2 " <<num <<endl;
-
-// cout<<"You Entered number is "<<num;
+//  cout<<rev;
+  
   return 0;
 }
