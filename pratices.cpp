e0563786667fc87759dda1881f979b436b616d25
deepak10100 <deepakmalakar98185@gmail.com>
2026-01-17 05:59:03 +0530

    learnning

--- pratices.cpp
+++ pratices.cpp
@@ -1,57 +1,67 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+// class Shop{
+//     int itemid[100];
+//     int price[100];
+//     int counter;
+//   public:
+//     void initCounter(){
+//       counter=0;
+//     }
+//     void setItem();
+//     void displayItems();
+
+// };
+// void Shop::setItem(){
+//   cout<<"Enter the Id ";
+//   cin>>itemid[counter];
+//   cout<<"Enter the Price ";
+//   cin>>price[counter];
+//   counter++;
+
+// }
+// void Shop::displayItems(){
+//   for (int i = 0; i < counter; i++)
+//   {
+//     cout<<"Your Product Id: "<<itemid[i]<<" Your Product Price "<<price[i]<<endl;
+//   }
+
+// }
+// int main(int argc, char const *argv[])
+// {
+//   Shop dukkan;
+//   dukkan.initCounter();
+//   dukkan.setItem();
+//   dukkan.setItem();
+//   dukkan.setItem();
+//   dukkan.displayItems();
+//   return 0;
+// }
+
+#include <iostream>
 using namespace std;
-class Binary{
-  private:
-    string s;
-  public:
-    void read();
-    void checkbin();
-    void onecom();
-    void display();
-};
-void Binary::read(){
-    cout<<"Enter the binary"<<endl;
-    cin>>s;
-}
-void Binary:: checkbin(){
-    for (int i = 0; i < s.length(); i++)
-    {
-      if (s.at(i)!='0' && s.at(i)!= '1')
+class Employee
+{
+    int id;
+    int salary;
+  public : 
+    void setId()
       {
-        cout<<"Incorrect Binary Number";
-        exit(0);
+        cout << "Enter the Id ";
+        cin >> id;
       }
-      
+    void printData(){
+      cout<<"Id is "<<id;
     }
-    
-}
-void Binary::onecom(){
-  for (int i = 0; i < s.length(); i++)
-  {
-    if (s.at(i)=='0')
-    {
-      s.at(i)='1';
-    }else{
-      s.at(i)='0';
-    }
-    
-  }
-  
-}
-void Binary::display(){
-  for (int i = 0; i < s.length(); i++)
-  {
-    cout<<s[i];
-  }
-  cout<<endl;
-}
+};
 int main(int argc, char const *argv[])
 {
-  Binary b;
-  b.read();
-  b.checkbin();
-  b.display();
-  b.onecom();
-  b.display();
+  Employee fb[4];
+  for (int i = 0; i < 4; i++)
+  {
+    fb[i].setId();
+    fb[i].printData();
+  }
+  
   return 0;
 }
