f0b89ef23c1022a2a6a844021a6d5bd9042899ae
deepak10100 <deepakmalakar98185@gmail.com>
2026-01-21 05:46:50 +0530

    leanrning

--- pratices.cpp
+++ pratices.cpp
@@ -75,19 +75,90 @@
 
 #include<iostream>
 using namespace std;
+#include<typeinfo>
 int main(int argc, char const *argv[])
 {
-  int n=-5;
-  if (n<0)
+  int num=765;
+  // int digit;
+  string res="";
+  while (num!=0)
   {
-    return n;
-  }else{
-    return -n ;
+    int digit=num%10;
+    int square=digit*digit;
+    num=num/10;
+   res= to_string(square)+res;
+    
   }
+  cout<<stoi(res);
+  // cout<<(type) num;
+  // int n=typeid(res);
+  // cout<<num;
+  // cout<<(int)res;
+  cout<< typeid(res).name();
+  
+  // int petrolDistance= 50;
+  // int onegallon = 25;
+  // int dis = 2*onegallon;
+  // if (dis>=petrolDistance)
+  // {
+  //   cout<<"true";
+  // }else
+  // {
+  //   cout<<"false";
+  // }
+  
+  
+  // bool employed=true;
+  // bool vacation = false;
+  // if (employed==true && vacation==false)
+  // {
+  //   cout<< "true";
+  // }
+  // else
+  // {
+  //   cout<<"false";
+  // }
+  
+// int bmi,weight,height;
+// weight=4;
+// height=2;
+// bmi = weight/height*height;
+// if (bmi<=18.5)
+// {
+//   return "Underweight";
+// }else if (bmi<=25.0)
+// {
+//   return "Normal";
+// }else if (bmi<=30.0)
+// {
+//   return"OverWeight";
+// }else
+// {
+//   return"Obese";
+// }
+
+
+
+
   
   
   return 0;
 }
+// #include<iostream>
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//   int n=-5;
+//   if (n<0)
+//   {
+//     return n;
+//   }else{
+//     return -n ;
+//   }
+  
+  
+//   return 0;
+// }
 // #include<iostream>
 // using namespace std;
 // int main(int argc, char const *argv[])
